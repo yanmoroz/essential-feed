708b80f08d5e288fccf96b8d85f3a542477f70a1
Yan Moroz <>
2023-11-22 01:37:10 +0300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Yan Moroz on 22.11.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
