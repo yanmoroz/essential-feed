f9ba8f5a40056913b9a56caf706bda14b8fa98c0
Yan Moroz <>
2023-11-26 04:39:47 +0300

    Move the URLSessionHTTPClient (the system under test, SUT) creation to a factory method to protect our tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -40,41 +40,43 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "http://any-url.com")!
         let expectation = expectation(description: "Wait for completion")
-
+        
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
             XCTAssertEqual(request.httpMethod, "GET")
             expectation.fulfill()
         }
-
-        URLSessionHTTPClient().get(from: url) { _ in }
-
+        
+        makeSUT().get(from: url) { _ in }
+        
         wait(for: [expectation], timeout: 1.0)
     }
     
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
-        URLProtocolStub.stub(data: nil, response: nil, error: error)
-
-        let sut = URLSessionHTTPClient()
         let expectation = expectation(description: "Wait for completion")
-
-        sut.get(from: url) { result in
+        URLProtocolStub.stub(data: nil, response: nil, error: error)
+        
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
             default:
                 XCTFail("Expected failure with error: \(error), got result: \(result) instead")
             }
-
+            
             expectation.fulfill()
         }
-
+        
         wait(for: [expectation], timeout: 1.0)
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
