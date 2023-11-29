31b58ec0adc3e4ebe9e8b3b6f60f427e50d38023
Yan Moroz <>
2023-11-30 01:42:51 +0300

    LocalFeedLoader does not message store upon creation (before loading the feed from the cache store)

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,7 +10,7 @@ import EssentialFeed
 
 final class CacheFeedUseCaseTests: XCTestCase {
     
-    func test_init_doesNotDeleteCacheUponCreation() {
+    func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
         
         XCTAssertEqual(store.receivedMessages, [])
