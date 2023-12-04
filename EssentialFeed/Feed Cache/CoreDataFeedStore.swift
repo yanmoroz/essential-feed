4234b9d7dc88d9fc626fec52720dd133540b154c
Yan Moroz <>
2023-12-05 01:25:50 +0300

    Load persistent container stores upon CoreDataFeedStore initialization

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -7,9 +7,41 @@
 
 import CoreData
 
+private extension NSPersistentContainer {
+    enum LoadingError: Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Error)
+    }
+    
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
+
 public class CoreDataFeedStore: FeedStore {
     
-    public init() {}
+    private let container: NSPersistentContainer
+    
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
