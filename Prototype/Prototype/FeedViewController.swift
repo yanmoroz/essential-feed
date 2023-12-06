ef23bbf0aa163d0765cdc63f40d1f10253661d79
Yan Moroz <>
2023-12-06 15:22:35 +0300

    Add 'FeedViewContoller' rendering x10 cells

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewControllerTableViewController.swift
+//  FeedViewController.swift
 //  Prototype
 //
 //  Created by Yan Moroz on 06.12.2023.
@@ -9,16 +9,8 @@ import UIKit
 
 class FeedViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
